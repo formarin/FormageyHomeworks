4eb9744fd05d656c042697c529444d041f81f61d
Irina <irina.formagey@gmail.com>
2021-08-01 03:48:04 +0300

    Created class Client, worked with IEnumerable

---
+++ Homework03/IEnumerable/IEnumerable/Client.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace IEnumerable
+{
+    public class Client
+    {
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string Patronymic { get; set; }
+        public int PasspotID { get; set; }
+        public double MoneyBalance { get; set; }
+        public Client(string surname, string name, string patronymic, int passpotID, double moneyBalance)
+        {
+            Surname = surname;
+            Name = name;
+            Patronymic = patronymic;
+            PasspotID = passpotID;
+            MoneyBalance = moneyBalance;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            if (!(obj is Client))
+            {
+                return false;
+            }
+            Client result = (Client)obj;
+            return result.PasspotID == PasspotID;
+        }
+
+        public static bool operator ==(Client first, Client second)
+        {
+            return first.Equals(second);
+        }
+        public static bool operator !=(Client first, Client second)
+        {
+            return !first.Equals(second);
+        }
+        public override int GetHashCode()
+        {
+            return Surname.GetHashCode() + Name.GetHashCode() + Patronymic.GetHashCode() + PasspotID.GetHashCode() + MoneyBalance.GetHashCode();
+        }
+    }
+}
---
+++ Homework03/IEnumerable/IEnumerable/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IEnumerable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Client> clientList = new List<Client>();
+            Random rnd = new Random();
+            int i = 0;
+
+            do
+            {
+                var client = new Client("Surname" + rnd.Next(1, 100), "Name" + rnd.Next(1, 100),
+                    "Patronymic" + rnd.Next(1, 100), rnd.Next(1000000, 10000000), rnd.Next(0, 10000000) / 100);
+                if (!(clientList.Contains(client)))
+                {
+                    clientList.Add(client);
+                }
+            } while (i < 1000000);
+
+            var searchedClient1 = SearchClientByPassportID(1975348, clientList);
+
+            var searchedClients2 = SearchClientByName("Name76", clientList);
+
+            var searchedClients3 = SearchClientsMoneyBalanceLowerThan(10000, clientList);
+
+            var searchedClient4 = SearchLowestMoneyBalanseClient(clientList);
+
+            var TotalMoneyBalance = CalculateTotalMoneyBalance(clientList);
+        }
+
+        public static IEnumerable<Client> SearchClientByPassportID(int pointedPassportID, List<Client> clientList)
+        {
+            return clientList.Where(client => client.PasspotID == pointedPassportID);
+        }
+
+        public static IEnumerable<Client> SearchClientByName(string pointedName, List<Client> clientList)
+        {
+            return clientList.Where(client => client.Name == pointedName);
+        }
+
+        public static IEnumerable<Client> SearchClientsMoneyBalanceLowerThan(double pointedBalance, List<Client> clientList)
+        {
+            return clientList.Where(client => client.MoneyBalance < pointedBalance);
+        }
+
+        public static IEnumerable<Client> SearchLowestMoneyBalanseClient(List<Client> clientList)
+        {
+            var LowestBalance = clientList.Min(client => client.MoneyBalance);
+            return clientList.Where(client => client.MoneyBalance == LowestBalance);
+        }
+
+        public static double CalculateTotalMoneyBalance(List<Client> clientList)
+        {
+            return clientList.Sum(client => client.MoneyBalance);
+        }
+    }
+}
