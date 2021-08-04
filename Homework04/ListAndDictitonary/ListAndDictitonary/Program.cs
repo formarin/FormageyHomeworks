90cca63dfbc89575857ea378573ec2affb93bb7c
Irina <irina.formagey@gmail.com>
2021-08-04 22:03:21 +0300

    Corrected mistakes.

--- Homework04/ListAndDictitonary/ListAndDictitonary/Program.cs
+++ Homework04/ListAndDictitonary/ListAndDictitonary/Program.cs
@@ -12,13 +12,13 @@ namespace ListAndDictitonary
             int i, j;
             var figure = new Figure();
             var figuresList = new List<Figure>();
-            var figuresDictionary = new Dictionary<int, Figure>();
+            var figuresDictionary = new Dictionary<Figure, int>();
 
             Stopwatch stopwatchL = new Stopwatch();
             Stopwatch stopwatchD = new Stopwatch();
             TimeSpan listSearchTime, dictionarySearchTime;
 
-            for (i = 1, j = 0; i <= 1000000; i++, j += 4)
+            for (i = 1, j = 0; i <= 10000; i++, j += 4)
             {
                 var circle = new Figure() { SideCount = 1, SideLength = i };
                 var triangle = new Figure() { SideCount = 3, SideLength = i };
@@ -27,20 +27,20 @@ namespace ListAndDictitonary
 
                 figuresList.AddRange(new Figure[] { circle, triangle, square, pentagon });
 
-                figuresDictionary.Add(j, circle);
-                figuresDictionary.Add(j + 1, triangle);
-                figuresDictionary.Add(j + 2, square);
-                figuresDictionary.Add(j + 3, pentagon);
+                figuresDictionary.Add(circle, j);
+                figuresDictionary.Add(triangle, j + 1);
+                figuresDictionary.Add(square, j + 2);
+                figuresDictionary.Add(pentagon, j + 3);
             }
 
-            for (i = 0; i < 1000000; i++)
+            for (i = 0; i < 10000; i++)
             {
                 stopwatchL.Start();
-                var circlesL = figuresList.Where(figures => figures.SideCount == 1);
+                var checkList = figuresList.Contains(new Figure() { SideCount = 1, SideLength = 500000 });
                 stopwatchL.Stop();
 
                 stopwatchD.Start();
-                var circlesD = figuresDictionary.Where(figures => figures.Value.SideCount == 1);
+                var checkDict = figuresDictionary.ContainsKey(new Figure() { SideCount = 1, SideLength = 500000 });
                 stopwatchD.Stop();
             }
 
