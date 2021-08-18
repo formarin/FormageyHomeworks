964bea7e5f8c07a0055cff7373d8988219bdcaaa
Irina <irina.formagey@gmail.com>
2021-08-18 09:26:51 +0300

    Worked with serialization and deserialization

---
+++ Serialization/Serialization/Figure.cs
@@ -0,0 +1,9 @@
+﻿namespace Serialization
+{
+    public class Figure
+    {
+        public int SideCount { get; set; }
+        public double SideLength { get; set; }
+        public Figure InsertedFigure { get; set; }
+    }
+}
---
+++ Serialization/Serialization/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle = new Figure() { SideCount = 1, SideLength = 1 };
+            var triangle = new Figure() { SideCount = 3, SideLength = 1 };
+            var square = new Figure() { SideCount = 4, SideLength = 1 };
+
+            var figList = new List<Figure>() { circle, triangle, square };
+            circle.InsertedFigure = square;
+
+            //Json
+            var serFigList = JsonConvert.SerializeObject(figList);
+            var desFiglist = JsonConvert.DeserializeObject<List<Figure>>(serFigList);
+
+            //XML
+            var formatter = new XmlSerializer(typeof(List<Figure>));
+            using (FileStream fs = new FileStream("figList.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, figList);
+            }
+            using (FileStream fs = new FileStream("figList.xml", FileMode.OpenOrCreate))
+            {
+                var desFigList = (List<Figure>)formatter.Deserialize(fs);
+            }
+
+            //Binary
+            using (FileStream fs = new FileStream("figList.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, figList);
+            }
+            using (FileStream fs = new FileStream("figList.dat", FileMode.OpenOrCreate))
+            {
+                var desFigList = (List<Figure>)formatter.Deserialize(fs);
+            }
+        }
+    }
+}
