d4f4562a4b36270671c48aa420dbf44cb9d25462
Irina <irina.formagey@gmail.com>
2021-08-18 20:34:22 +0300

    Corrected mistakes

--- Serialization/Serialization/Figure.cs
+++ Serialization/Serialization/Figure.cs
@@ -1,9 +1,14 @@
-﻿namespace Serialization
+﻿using System;
+
+namespace Serialization
 {
+    [Serializable]
     public class Figure
     {
         public int SideCount { get; set; }
         public double SideLength { get; set; }
         public Figure InsertedFigure { get; set; }
+
+        public Figure() { }
     }
 }
