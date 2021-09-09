d6aa9c7ca6471eaefebcdb9bf3576fa09cdd3a57
formarin <irina.formagey@gmail.com>
2021-09-09 13:18:46 +0300

    Corrected work with threads

--- Threads/Threads/Threads/Program.cs
+++ Threads/Threads/Threads/Program.cs
@@ -1,119 +1,71 @@
 ﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Threading;
+using System.Diagnostics;
 using System.Threading.Tasks;
 
 namespace Threads
 {
     class Program
     {
+        public static int length = 100_000_000;
+        public static int[] array = new int[length];
+        public static int sum = 0;
+
         static void Main(string[] args)
         {
             //Генерировать и считать среднее арифметическое для массива размером в 10М и 100М элементов.
             //Реализовать 2 варианта, параллельные вычисления и без них, оценить результаты.
+
+            int i, n = 10;
+            Random rnd = new Random();
+            double average1, average2;
+            var stopwatch = new Stopwatch();
+            TimeSpan sequentialCalculationsTime, parallelCalculationsTime;
+
+            for (i = 0; i < array.Length; i++)
             {
-                //var locker = new object();
-                //var rnd = new Random();
-                //int l = 10_000/*_000*/, w = 100_000/*_000*/;
-                //int i, j;
-                //var completed = 0;
-
-                //var intArray = new int[l, w];
-                //double average;
-                //int sum = 0;
-
-                ////вариант 1
-                ////параллельная генерация массива и вычисления
-
-                ////генерирует содержимое массива
-                //ThreadPool.QueueUserWorkItem(_ =>
-                //{
-                //    for (i = 0; i < intArray.Length; i++)
-                //    {
-                //        lock (locker)
-                //        {
-                //            intArray[i % l, i / l] = rnd.Next(5, 10);
-                //            Console.WriteLine($"{i + 1} elements");
-                //        }
-                //    }
-                //    Interlocked.Increment(ref completed);
-                //});
-
-                ////считает среднее арифметическое
-                //ThreadPool.QueueUserWorkItem(_ =>
-                //{
-                //    for (j = 0; j < intArray.Length; j++)
-                //    {
-                //        lock (locker)
-                //        {
-                //            sum += intArray[j % l, j / l];
-                //            average = Convert.ToDouble(sum) / (j + 1);
-                //            Console.WriteLine($"current average:  {average:n4}");
-                //        }
-                //    }
-                //    Interlocked.Increment(ref completed);
-                //});
-
-                ////ожидает завершение работы потоков
-                //while (completed < 2)
-                //{
-                //    Thread.Sleep(500);
-                //}
-
-                ////вариант 2
-                ////считает среднее арифметическое после генерации массива
-                ////(т.к. массив уже сгенерирован, осталось лишь посчитать среднее арифметическое)
-                //sum = 0;
-                //foreach (var item in intArray)
-                //{
-                //    sum += item;
-                //}
-                //average = Convert.ToDouble(sum) / intArray.Length;
-                //Console.WriteLine($"\nreal average:  {average}");
-
-                ////Оценка результата: при параллельных генерации и вычислениях поток, вычисляющий среднее арифметическое
-                ////элементов массива, может "уйти вперёд" => результат вычислений исказится 
+                array[i] = rnd.Next(5, 10);
             }
 
-            //Реализовать очередь задач, которая позволит добавлять задачи на исполнение из разных
-            //потоков одновременно и регулировать кол-во одновременно выполняемых задач.
-
-            var taskRecieverAndExecutor = new TaskRecieverAndExecutor() { TaskQueue = new List<Task>(),
-                CancelTokenSource= new CancellationTokenSource()};
-            taskRecieverAndExecutor.Token = taskRecieverAndExecutor.CancelTokenSource.Token;
-
-            ThreadPool.QueueUserWorkItem(_ =>
+            //последовательные вычисления
+            stopwatch.Start();
+            for (i = 0; i < n; i++)
             {
-                while (true)
+                sum = 0;
+                foreach (var item in array)
                 {
-                    if (taskRecieverAndExecutor.TaskQueue.Count < 100)
-                    {
-                        taskRecieverAndExecutor.AddTask(() => 
-                        {
-                            Console.WriteLine($"New task started executing..");
-                            Thread.Sleep(50);
-                            Console.WriteLine($"      Task finished executing");
-                        });
-                    }
+                    sum += item;
                 }
-            });
+                average1 = Convert.ToDouble(sum) / length;
+            }
+            stopwatch.Stop();
+            sequentialCalculationsTime = stopwatch.Elapsed / n;
 
-            ThreadPool.QueueUserWorkItem(_ =>
+            stopwatch = new Stopwatch();
+
+            //параллельные вычисления
+            stopwatch.Start();
+
+            for (i = 0; i < n; i++)
             {
-                taskRecieverAndExecutor.Start(20);
-            });
+                sum = 0;
+                Parallel.For(0, length / 4 - 1, GetSum);
+                Parallel.For(length / 4, length / 2 - 1, GetSum);
+                Parallel.For(length / 2, length / 4 * 3 - 1, GetSum);
+                Parallel.For(length / 4 * 3, length - 1, GetSum);
+                average2 = Convert.ToDouble(sum) / length;
+            }
+            stopwatch.Stop();
+            parallelCalculationsTime = stopwatch.Elapsed / n;
 
-            ThreadPool.QueueUserWorkItem(_ =>
-            {
-                Thread.Sleep(1000);
-                taskRecieverAndExecutor.Stop();
-                taskRecieverAndExecutor.Clear();
-
-                Console.WriteLine($"-----------------Stop---------------");
-            });
+            Console.WriteLine($"{sequentialCalculationsTime} - время последовательных вычислений" +
+                $"\n{parallelCalculationsTime} - время параллельных вычислений");
 
             Console.ReadKey();
+        }
+
+        public static void GetSum(int i)
+        {
+            sum += array[i];
         }
     }
 }
